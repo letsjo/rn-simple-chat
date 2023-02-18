3762025b222f35f796a7b7f330c222213409d25b
letsjo <hyunoh.jo@gmail.com>
2023-02-19 01:40:31 +0900

    feat(theme): style theme init

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+const color = {
+  white: '#ffffff',
+  black: '#000000',
+  gray_0: '#f5f5f5',
+  gray_1: '#a6a6a6',
+  red: '#e84118',
+  blue: '#3679fe',
+};
+
+export const theme = {
+  background: color.white,
+  text: color.black,
+};
