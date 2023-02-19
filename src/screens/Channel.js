1d3c278ebf1fcf9f5266dfdb8610fd65ce1f3427
letsjo <hyunoh.jo@gmail.com>
2023-02-20 01:50:38 +0900

    feat(screens/Channel): 메세지 데이터 리스트 init

--- src/screens/Channel.js
+++ src/screens/Channel.js
@@ -1,21 +1,56 @@
-import React from 'react'
+import { useEffect, useLayoutEffect, useState } from 'react'
 import styled from 'styled-components/native'
-import { Text } from 'react-native'
+import { Text, FlatList } from 'react-native'
+import { createMessage, getCurrentUser, app } from '../utils/firebase';
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+  query,
+  doc,
+  orderBy,
+} from 'firebase/firestore';
 
 const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.background};
 `
 
-const Channel = ({ route }) => {
+const Channel = ({ navigation, route: { params } }) => {
+  const [messages, setMessages] = useState([]);
+
+  const db = getFirestore(app);
+  useEffect(() => {
+    const docRef = doc(db, 'channels', route.params.id);
+    const collectionQuery = query(
+      collection(db, `${docRef.path}/messages`),
+      orderBy('createdAt', 'desc')
+    );
+    const unsubscribe = onSnapshot(collectionQuery, snapshot => {
+      const list = [];
+      snapshot.forEach(doc => {
+        list.push(doc.data());
+      });
+      setMessages(list);
+    });
+    return () => unsubscribe();
+  })
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: params.title || 'Channel',
+    });
+  }, [])
+
   return (
     <Container>
-      <Text
-        style={ { fontSize: 24 } }
-      >ID: { route.params?.id }</Text>
-      <Text
-        style={ { fontSize: 24 } }
-      >Title: { route.params?.title }</Text>
+      <FlatList
+        keyExtractor={ item => item['id'] }
+        data={ messages }
+        renderItem={ ({ item }) => (
+          <Text style={ { fontSize: 24 } }>{ item.text }</Text>
+        ) }
+      />
     </Container>
   )
 }
